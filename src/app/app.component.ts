fa33a9f047701a4f6ddf04d12c8b0f2560bb06d1
joseriosjose <jose_rios456@hotmail.com>
2020-11-26 14:40:15 -0600

    Commit con crud angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+
+//importando la clase empleado 
+import { Empleado } from './models/empleado/empleado'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'simpleCrudAngular';
+  arregloEmpleados: Empleado[] = [
+    { id: 1, nombre: "jose", ciudad: "oaxaca" },
+    { id: 2, nombre: "pedro", ciudad: "veracruz" },
+  ]
+
+  empleadoSelecionado: Empleado = new Empleado();
+
+  agregaroeditar() {
+    if (this.empleadoSelecionado.id === 0) {
+      this.empleadoSelecionado.id = this.arregloEmpleados.length + 1;
+      this.arregloEmpleados.push(this.empleadoSelecionado);
+    }
+    this.empleadoSelecionado = new Empleado();
+
+  }
+
+  abrirparaeditar(empleado: Empleado) {
+    this.empleadoSelecionado = empleado;
+  }
+
+  eliminar() {
+    if (confirm(`estas seguro que quieres eliminar a ${this.empleadoSelecionado.nombre}`)) {
+      this.arregloEmpleados = this.arregloEmpleados.filter(item => item != this.empleadoSelecionado);
+      this.empleadoSelecionado = new Empleado();
+    }
+
+  }
+
 }
